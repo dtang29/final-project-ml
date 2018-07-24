729badc3ea2961d361ec3a9ca0abadc83b088d6c
Daniel Tang <dtang1529@gmail.com>
2018-07-24 14:12:18 -0700

    added boiler plate code to index.html and app.py

--- app.py
+++ app.py
@@ -9,3 +9,13 @@ from bson import json_util
 
 app = Flask(__name__)
 mongo = PyMongo(app)
+
+
+# create route that renders index.html template
+@app.route("/")
+def echo():
+    return render_template("index.html", text="Serving up cool text from the Flask server!!")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
